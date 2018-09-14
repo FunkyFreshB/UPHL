ad7aa10b46891da4fae46c536f270c08b2d0d57d
NieRKernel <jonatansvenjeby@yahoo.se>
2018-09-14 16:02:23 +0200

    Created gaze cursor and simple graphics
    Cursor, holograms creation and coidng

---
+++ JonatanSDemo/Assets/WorldCursor.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+public class WorldCursor : MonoBehaviour {
+
+    private MeshRenderer cursor;
+
+	// Use this for initialization
+	void Start () {
+        cursor = this.gameObject.GetComponentInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var headPos = Camera.main.transform.position;
+        var gazeDir = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(headPos,gazeDir, out hitInfo))
+        {
+            cursor.enabled = true;
+
+            this.transform.position = hitInfo.point;
+
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+
+        else
+        {
+            cursor.enabled = false;
+        }
+	}
+}

2f56e80cf852f39477cec3ae719ce88ee8721d21
FunkyFreshB <sebastian.sela@hotmail.com>
2018-09-18 00:52:05 +0200

    Värlskarta v.2 + Bucket v.2
    - AR_PROJ_1 kommer nu heta AR Demo - Place, Pop & Drag nästa gång vi buildar in i HL:n
    - Ändrat Hold i båda projekt så de förhoppningsvis funkar rätt
    - Lite andra småändringar i f.d. AR_PROJ_1

---
+++ AR_PROJ_1_LTS/Assets/S_DraggableObj.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_DraggableObj : MonoBehaviour {
+
+    bool holding = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnHoldStart()
+    {
+        holding = true;
+    }
+
+    void OnHoldCompleted()
+    {
+        holding = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (holding)
+        {
+            transform.position = Camera.main.GetComponent<S_UserInteraction>().dragPos;
+        }
+	}
+}

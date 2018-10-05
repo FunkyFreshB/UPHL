bbe3267a4ed89a8b68c103c1a177fea7e14ddf07
FunkyFreshB <sebastian.sela@hotmail.com>
2018-10-05 12:23:46 +0200

    Legacy
    The following demos are classified as legacy:
    - AR Demo - Decorating
    - AR_PROJ_1_LTS
    - Bucket Demo
    
    Holo Helper v.00001 eller nåt has been added.

---
+++ Holo Helper/Assets/ActivityManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivityManager : MonoBehaviour {
+
+    public GameObject buttonBase;
+    public ArrayList activities = new ArrayList();
+    private int noOfActivities = 0;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    //    this.transform.LookAt(Camera.main.transform);
+	}
+
+    public GameObject CreateActivity(string name)
+    {
+        GameObject newActivity = Instantiate(buttonBase);
+        newActivity.name = name;
+        newActivity.GetComponentInChildren<TextMesh>().text = name;
+        newActivity.transform.position = this.transform.position;
+        
+        activities.Add(newActivity);
+        if (activities.Count > noOfActivities)
+        {
+            noOfActivities++;
+            Debug.Log("Added " + name);
+            return newActivity;
+        }
+        else
+        {
+            Debug.Log("Added NOTHING!?");
+            return null;
+        }
+    }
+    public bool DeleteActivity(int index)
+    {
+         activities.RemoveAt(index);
+        
+        if(activities.Count < noOfActivities)
+        {
+            noOfActivities--;
+            Debug.Log("Removed " + name);
+            return true;
+        }
+        else
+        {
+            Debug.Log("Removed NOTHING!?");
+            return false;
+        }
+    }
+}
---
+++ Holo Helper/Assets/ButtonBehaviour.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.XR.WSA.Input;
+
+public class ButtonBehaviour : MonoBehaviour, IInputClickHandler, IFocusable {
+
+    public GameObject actMan;
+    public GameObject[] menus = new GameObject[5];
+    public Material[] materials = new Material[2];
+    GameObject obj;
+    int activityNo = 1;
+
+    public bool isAdmin;
+    public bool isActivity = false;
+    public bool isEdit = false;
+    public bool isCreate = false;
+    public bool isDelete = false;
+    public bool isReturn = false;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        // Unhighlight the highlighted button
+        this.gameObject.GetComponent<Renderer>().material = materials[0];
+
+        // 0: Main Menu
+        if (menus[0].activeSelf)
+        {
+            if (isAdmin)
+            {
+                menus[1].SetActive(true);
+                menus[0].SetActive(false);
+            }
+            else
+            {
+                menus[2].SetActive(true);
+                menus[0].SetActive(false);
+            }
+        }
+
+        // ---------------------------------------------
+
+        // 1: Administrator Menu
+        else if (menus[1].activeSelf)
+        {
+            if (isAdmin && isCreate)
+            {
+                // TANGENTBORD HÄR
+                obj = GetComponentInParent<ActivityManager>().CreateActivity("name " + activityNo);
+                obj.transform.SetParent(actMan.transform);
+                activityNo++;
+            }
+            if (isAdmin && isEdit)
+            {
+                // VAD SKA GÖRAS HÄR
+            }
+            if (isAdmin && isDelete)
+            {
+                menus[3].SetActive(true);
+                menus[1].SetActive(false);
+            }
+            if (isReturn)
+            {
+                menus[0].SetActive(true);
+                menus[1].SetActive(false);
+            }
+        }
+
+        // ---------------------------------------------
+
+        // 2: Activity Menu
+        else if (menus[2].activeSelf)
+        {
+            if (isActivity)
+            {
+                GetComponentInParent<ActivityManager>().activities.IndexOf(23);
+                GetComponentInChildren<TextMesh>().text = obj.name;
+            }
+        }
+
+        // ---------------------------------------------
+
+        // 3: Delete Menu
+        else if (menus[3].activeSelf)
+        {
+
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnFocusEnter()
+    {
+        this.gameObject.GetComponent<Renderer>().material = materials[1];
+    }
+
+    public void OnFocusExit()
+    {
+        this.gameObject.GetComponent<Renderer>().material = materials[0];
+    }
+}

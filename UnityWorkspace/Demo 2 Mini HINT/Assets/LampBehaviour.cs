81a33a2667c1f0a8c5a32ddf08d090d25faac481
Richovic <43266282+Richovic@users.noreply.github.com>
2018-10-16 12:52:51 +0200

    Added projects
    Added Demo 2 mini HINT & menuTest

---
+++ UnityWorkspace/Demo 2 Mini HINT/Assets/LampBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampBehaviour : MonoBehaviour {
+
+    [Tooltip("Lamp to turn on")]
+    [SerializeField]
+    private Light lightSource;
+    
+	// Use this for initialization
+	void Start () {
+        if (lightSource == null)
+            throw new System.NullReferenceException();
+	}
+
+    public void LightOn() {
+        lightSource.enabled = true;
+    }
+
+    public void LightOff() {
+        lightSource.enabled = false;
+    }
+}
---
+++ UnityWorkspace/Demo 2 Mini HINT/Assets/SensorInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorInput : MonoBehaviour {
+
+    Material activeMaterial;
+    Material cachedMaterial;
+
+    void Start() {
+        activeMaterial.color = Color.red;
+        cachedMaterial = GetComponent<Renderer>().material;
+    }
+
+    public void activate() {
+        GetComponent<Renderer>().material = activeMaterial;
+    }
+
+    public void deactivate() {
+        GetComponent<Renderer>().material = cachedMaterial;
+    }
+}

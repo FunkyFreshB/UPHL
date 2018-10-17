f4062ef1212725835b0d134961a5ef59181a671c
Richovic <43266282+Richovic@users.noreply.github.com>
2018-10-17 16:48:07 +0200

    No changes

--- UnityWorkspace/Demo 2 Mini HINT/Assets/Move_Rotate_Scale.cs
+++ UnityWorkspace/Demo 2 Mini HINT/Assets/Move_Rotate_Scale.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoloToolkit.Unity.InputModule;
 
-public class Move_Rotate_Scale : MonoBehaviour {
+public class Move_Rotate_Scale : MonoBehaviour, IFocusable, IInputHandler, ISourceStateHandler {
+    public void OnFocusEnter() {
+        throw new System.NotImplementedException();
+    }
 
-	// Use this for initialization
-	void Start () {
+    public void OnFocusExit() {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInputDown(InputEventData eventData) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInputUp(InputEventData eventData) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSourceDetected(SourceStateEventData eventData) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSourceLost(SourceStateEventData eventData) {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	

8a0bf37e49ebbb2680bb7af4e7701ebd028a9283
NieRKernel <jonatansvenjeby@yahoo.se>
2018-10-18 12:32:12 +0200

    Fixed MHINT
    Handraggable on only the walls and floors.

--- UnityWorkspace/Demo 2 Mini HINT/Assets/Move_Rotate_Scale.cs
+++ UnityWorkspace/Demo 2 Mini HINT/Assets/Move_Rotate_Scale.cs
@@ -1,35 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using HoloToolkit.Unity.InputModule;
 
-public class Move_Rotate_Scale : MonoBehaviour, IFocusable, IInputHandler, ISourceStateHandler {
-    public void OnFocusEnter() {
-        throw new System.NotImplementedException();
-    }
+public class Move_Rotate_Scale : MonoBehaviour {
 
-    public void OnFocusExit() {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnInputDown(InputEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnInputUp(InputEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnSourceDetected(SourceStateEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnSourceLost(SourceStateEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 		
 	}
 	

57e3e51723fecc29b186da11ce964c37a92ab200
NieRKernel <jonatansvenjeby@yahoo.se>
2018-11-09 16:48:12 +0100

    databas successs

--- UnityWorkspace/Database test/Database test/Assets/Database.cs
+++ UnityWorkspace/Database test/Database test/Assets/Database.cs
@@ -1,14 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Npgsql;
+using System;
 
 
 public class Database : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+        string connectionString = "Server=192.168.1.198;" +
+          "Database=idb4home;" +
+          "User ID=postgres;" +
+          "Password=postgres;";
+
+        NpgsqlConnection dbcon;
+        dbcon = new NpgsqlConnection(connectionString);
+        dbcon.Open();
+
+        NpgsqlCommand dbcmd = dbcon.CreateCommand();
+
+        string sql = "SELECT * " + "FROM resource";
+
+        dbcmd.CommandText = sql;
+        NpgsqlDataReader reader = dbcmd.ExecuteReader();
+        string text = "";
+        while (reader.Read()) {
+            string FirstName = "";// (string)reader["resource_uuid"];
+            string LastName = (string)reader["description"];
+            text += " " + LastName;
+        }
+        this.GetComponent<TextMesh>().text = text;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,24 +41,5 @@ public class Database : MonoBehaviour {
 
 }
 
-var connString = "Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase";
+//var connString = "Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase";
 
-using (var conn = new NpgsqlConnection(connString))
-{
-    conn.Open();
-
-    // Insert some data
-    using (var cmd = new NpgsqlCommand())
-    {
-        cmd.Connection = conn;
-        cmd.CommandText = "INSERT INTO data (some_field) VALUES (@p)";
-        cmd.Parameters.AddWithValue("p", "Hello world");
-        cmd.ExecuteNonQuery();
-    }
-
-    // Retrieve all rows
-    using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
-    using (var reader = cmd.ExecuteReader())
-        while (reader.Read())
-            Console.WriteLine(reader.GetString(0));
-}

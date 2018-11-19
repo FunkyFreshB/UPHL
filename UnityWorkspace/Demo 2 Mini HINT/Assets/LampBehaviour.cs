f9c4cab0763dc2adf34447b7cafc58b173c72bc2
NieRKernel <jonatansvenjeby@yahoo.se>
2018-11-19 15:26:06 +0100

    Update on Mini HINT
    Added colours to the model.
    Rewrote buttonBehaviour

--- UnityWorkspace/Demo 2 Mini HINT/Assets/LampBehaviour.cs
+++ UnityWorkspace/Demo 2 Mini HINT/Assets/LampBehaviour.cs
@@ -1,24 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoloToolkit.Unity.InputModule;
 
-public class LampBehaviour : MonoBehaviour {
+public class LampBehaviour : MonoBehaviour, IInputClickHandler {
 
     [Tooltip("Lamp to turn on")]
-    [SerializeField]
-    private Light lightSource;
-    
-	// Use this for initialization
-	void Start () {
+    public Light lightSource;
+
+    private Material lampOn;
+    private Material lampOff;
+
+    private static float COOLDOWN = 1.0f;
+    private float timer = 0.0f;
+    private bool isTurnedOn = false;
+
+    void Start() {
         if (lightSource == null)
             throw new System.NullReferenceException();
-	}
-
-    public void LightOn() {
-        lightSource.enabled = true;
+        lampOn = new Material(Shader.Find("Specular"));
+        lampOn.color = Color.green;
+        lampOff = new Material(Shader.Find("Specular"));
+        lampOff.color = Color.red;
+        timer = COOLDOWN;
+        //OnInputClicked(null);
     }
 
-    public void LightOff() {
-        lightSource.enabled = false;
+    void Update() {
+        timer -= Time.deltaTime;
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData) {
+        if (timer < 0) {
+            timer = COOLDOWN;
+            if (!isTurnedOn) {
+                isTurnedOn = true;
+                lightSource.enabled = true;
+                gameObject.GetComponent<Renderer>().material = lampOn;
+            }
+            else {
+                isTurnedOn = false;
+                lightSource.enabled = false;
+                gameObject.GetComponent<Renderer>().material = lampOff;
+            }
+        }
     }
 }

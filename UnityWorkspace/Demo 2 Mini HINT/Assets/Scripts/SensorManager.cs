8c551b84181de5ecbbc4855366ce739d313d35a1
NieRKernel <jonatansvenjeby@yahoo.se>
2018-12-21 17:37:18 +0100

    Update on MiniHINT
    The model should react to every implemeted sensor. motion-, magnetic sensor and buttons.

--- UnityWorkspace/Demo 2 Mini HINT/Assets/Scripts/SensorManager.cs
+++ UnityWorkspace/Demo 2 Mini HINT/Assets/Scripts/SensorManager.cs
@@ -7,70 +7,6 @@ using HoloToolkit.UX.ToolTips;
 
 public class SensorManager : MonoBehaviour {
 
-    /*
-    public GameObject Bedroom_Bed;
-    public GameObject Bedroom_Chair_1;
-    public GameObject Bedroom_Chair_2;
-    public GameObject Bedroom_Dresser_1_Drawer_1;
-    public GameObject Bedroom_Dresser_1_Drawer_2;
-    public GameObject Bedroom_Dresser_1_Drawer_3;
-    public GameObject Bedroom_Dresser_1_Drawer_4;
-    public GameObject Bedroom_Dresser_2_Drawer_1;
-    public GameObject Bedroom_Dresser_2_Drawer_2;
-    public GameObject Bedroom_Dresser_2_Drawer_3;
-    public GameObject Bedroom_Dresser_2_Drawer_4;
-    public GameObject Bedroom_Dresser_2_Drawer_5;
-    public GameObject Bedroom_Dresser_2_Drawer_6;
-    public GameObject Bedroom_Dresser_3_Drawer_1;
-    public GameObject Bedroom_Dresser_3_Drawer_2;
-    public GameObject Bedroom_Dresser_3_Drawer_3;
-    public GameObject Bedroom_Dresser_3_Drawer_4;
-    public GameObject Bedroom_Dresser_3_Drawer_5;
-    public GameObject Bedroom_Nightstand_Drawer;
-    public GameObject Floor_Bathroom;
-    public GameObject Floor_Bedroom;
-    public GameObject Floor_Kitchen;
-    public GameObject Floor_Livingroom_1;
-    public GameObject Floor_Livingroom_2;
-    public GameObject Floor_Livingroom_3;
-    public GameObject Kitchen_Left_Cabinet_1;
-    public GameObject Kitchen_Left_Cabinet_2;
-    public GameObject Kitchen_Left_Cabinet_3;
-    public GameObject Kitchen_Left_Cabinet_4;
-    public GameObject Kitchen_Left_Dishwasher;
-    public GameObject Kitchen_Left_Drawer_1;
-    public GameObject Kitchen_Left_Drawer_2;
-    public GameObject Kitchen_Left_Drawer_3;
-    public GameObject Kitchen_Refridgerator;
-    public GameObject Lampbutton_Bathroom;
-    public GameObject Lampbutton_Bedroom;
-    public GameObject Lampbutton_Kitchen;
-    public GameObject Lampbutton_Livingroom_1;
-    public GameObject Lampbutton_Livingroom_2;
-    public GameObject Lampbutton_Livingroom_3;
-    public GameObject Livingroom_Chair_1;
-    public GameObject Livingroom_Chair_2;
-    public GameObject Livingroom_Chair_Cover_1;
-    public GameObject Livingroom_Chair_Cover_2;
-    public GameObject Livingroom_Chair_Cover_3;
-    public GameObject Livingroom_Chair_Cover_4;
-    public GameObject Livingroom_Sofa_Cushion_1;
-    public GameObject Livingroom_Sofa_Cushion_2;
-    public GameObject Livingroom_Sofa_Cushion_3;
-    public GameObject Livingroom_Sofa_Cushion_1_1;
-    public GameObject Livingroom_Sofa_Cushion_2_1;
-    public GameObject Livingroom_Sofa_Cushion_2_2;
-    public GameObject Livingroom_Sofa_Cushion_3_1;
-    public GameObject Livingroom_Stool_1_Cushion;
-    public GameObject Livingroom_Stool_2_Cushion;
-    public GameObject Light_Bathroom;
-    public GameObject Light_Bedroom;
-    public GameObject Light_kitchen;
-    public GameObject Light_Livingroom;
-    public GameObject Light_Table;
-    public GameObject Light_StorageArea;
-    */
-
     public Material lampOn;
     public Material lampOff;
     public Material sensorOn;
@@ -95,6 +31,11 @@ public class SensorManager : MonoBehaviour {
         timer = 2.0f;
         lastUpdateTime = "";
         //a78a81027d6e4fa5980ec51d32598212
+
+
+        //Sync model with reality
+        WWW request = new WWW("http://192.168.1.195:8080/getevents.py?ts=2000-01-01T00:00:00");
+        StartCoroutine(OnResponse(request));
     }
 
     private void SetupSensorList() {
@@ -137,13 +78,18 @@ public class SensorManager : MonoBehaviour {
     public IEnumerator OnResponse(WWW req) {
         yield return req;
         UpdateSensors(ProcessJSON(req.text));
+
+        
     }
 
-    private class SensorData {
+
+    [System.Serializable]
+    public class SensorData {
         public String resource;
         public bool sample;
         public String db_time_stamp;
     }
+    
 
     private SensorData[] ProcessJSON(string jsonString) {
 
@@ -165,11 +111,6 @@ public class SensorManager : MonoBehaviour {
             lastUpdateTime = lastUpdateTime.Remove(19, data.db_time_stamp.Length - 19);
             data.db_time_stamp = lastUpdateTime;
 
-            Debug.Log("Iteration: " + i);
-            Debug.Log("Resource: " + data.resource);
-            Debug.Log("Sample: " + data.sample);
-            Debug.Log("Time: " + data.db_time_stamp);
-
             dataArray[i] = data;
 
         }
@@ -178,10 +119,17 @@ public class SensorManager : MonoBehaviour {
 
     private void UpdateSensors(SensorData[] dataArray) {
 
+        
+
         foreach (SensorData data in dataArray) {
             if (data.resource != null) {
+                
+
                 foreach (GameObject sensorObject in sensorList) {
                     Sensor sensor = sensorObject.GetComponent<Sensor>();
+
+                    
+
                     if (data.resource.Equals(sensor.resource)) {
                         sensor.sample = data.sample;
                         sensor.db_time_stamp = data.db_time_stamp;
@@ -204,4 +152,8 @@ public class SensorManager : MonoBehaviour {
             }
         }
     }
+
+
+
+    
 }
